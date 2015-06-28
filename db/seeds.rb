6bdfbdca46a37cd735280002673757009434c980
gregoryhbowler <greg@brudmusic.com>
2015-06-27 18:39:42 -0700

    Assignment 32 Seed Data completed, all problems fixed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,46 +1,69 @@
+ #  require 'faker'
+ 
+ # # Create Posts
+ # 50.times do
+ #   Post.create!(
+ #     title:  Faker::Lorem.sentence,
+ #     body:   Faker::Lorem.paragraph
+ #   )
+ # end
+ # posts = Post.all
+ 
+ # # Create Comments
+ # 100.times do
+ #   Comment.create!(
+ #     post: posts.sample,
+ #     body: Faker::Lorem.paragraph
+ #   )
+ # end
+ 
+ # puts "Seed finished"
+ # puts "#{Post.count} posts created"
+ # puts "#{Comment.count} comments created"
+
  require 'faker'
  
- # Create Posts
- 50.times do
-   Post.create!(
-     title:  Faker::Lorem.sentence,
-     body:   Faker::Lorem.paragraph
-   )
- end
+ # # Create Posts
+ # 50.times do
+ #   Post.create!(
+ #     title:  Faker::Lorem.sentence,
+ #     body:   Faker::Lorem.paragraph
+ #   )
+ # end
 
- posts = Post.all
+
 
  
- # Create Comments
- 100.times do
-   Comment.create!(
-     post: posts.sample,
-     body: Faker::Lorem.paragraph
-   )
- end
+ # # Create Comments
+ # 100.times do
+ #   Comment.create!(
+ #     post: posts.sample,
+ #     body: Faker::Lorem.paragraph
+ #   )
+ # end
 
 
-#  1.times do
-#   postExist = Post.find_by title: 'title'
-#   if postExist.nil?
-#     Post.create!(
-#   title: "Blade Runner",
-#   body: "Do Androids Dream of Electric Sheep?"
-#   )
-#   end
-# end
+ 1.times do
+  postExist = Post.find_by title: 'title'
+  if postExist.nil?
+    Post.create!(
+  title: "Blade Runner",
+  body: "Do Androids Dream of Electric Sheep?"
+  )
+  end
+end
 
+posts = Post.all
 
-
-# 1.times do
-#   commentExist = Comment.find_by body: 'comment'
-#   if commentExist.nil?
-#  Comment.create!(
-#     post: posts.sample,
-#     body: "which one is better?"
-#   )
-#   end
-# end
+1.times do
+  commentExist = Comment.find_by body: 'comment'
+  if commentExist.nil?
+ Comment.create!(
+    post: posts.sample,
+    body: "which one is better?"
+  )
+  end
+end
 
 
  puts "Seed finished"
