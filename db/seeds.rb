16c308ea9f5ea04cdc17dd1e217d77960fea9aa7
gregoryhbowler <greg@brudmusic.com>
2015-06-27 17:42:11 -0700

    having an issue with rake. will not run. created db seed files but cannot create with rake.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,9 @@
      body:   Faker::Lorem.paragraph
    )
  end
+
  posts = Post.all
+
  
  # Create Comments
  100.times do
@@ -16,7 +18,31 @@
      body: Faker::Lorem.paragraph
    )
  end
- 
+
+
+#  1.times do
+#   postExist = Post.find_by title: 'title'
+#   if postExist.nil?
+#     Post.create!(
+#   title: "Blade Runner",
+#   body: "Do Androids Dream of Electric Sheep?"
+#   )
+#   end
+# end
+
+
+
+# 1.times do
+#   commentExist = Comment.find_by body: 'comment'
+#   if commentExist.nil?
+#  Comment.create!(
+#     post: posts.sample,
+#     body: "which one is better?"
+#   )
+#   end
+# end
+
+
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
