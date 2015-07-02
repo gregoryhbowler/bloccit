2effc5b75a1c97f0899458ffafbb0120c8b812d5
gregoryhbowler <greg@brudmusic.com>
2015-07-02 10:14:14 -0700

    assignment 39 authorization complete

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -2,4 +2,21 @@ class PostPolicy < ApplicationPolicy
   def index?
     true
   end
+
+  class Scope < Scope
+
+    def resolve
+#      if user.admin? || user.moderator?
+#        scope.all
+#      else
+#        scope.where(user: user)
+      if user.blank?
+        scope.none
+      elsif user.admin? || user.moderator?
+        scope.all
+      else
+        scope.where( user: user)
+      end
+    end
+  end
 end
