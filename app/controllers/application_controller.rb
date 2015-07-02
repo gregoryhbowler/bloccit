8075500261bea907ed2cb727d8a993f3e029f3ae
gregoryhbowler <greg@brudmusic.com>
2015-07-01 17:38:49 -0700

    assignment 37 user authentication continued is complete, added a before_action filter and a skip_before_action filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,20 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  before_action :configure_permited_paramaters, if: :devise_controller?
+# Prevent CSRF attacks by raising an exception.
+# For APIs, you may want to use :null_session instead.
+protect_from_forgery with: :exception
+before_action :configure_permited_paramaters, if: :devise_controller?  
+before_action :flash_attack
 
-  protected
+
+protected
+
+  def flash_attack
+    flash[:notice] = "Uh oh you got flashed"
+  end
+
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) >> :name
   end
+
 end
