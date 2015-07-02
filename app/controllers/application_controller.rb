ff04019cb23b330dfb6defbc07465a97aafd85e0
gregoryhbowler <greg@brudmusic.com>
2015-07-02 12:57:17 -0700

    updated flash attack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 # For APIs, you may want to use :null_session instead.
 protect_from_forgery with: :exception
 before_action :configure_permited_paramaters, if: :devise_controller?  
-before_action :flash_attack
+
 
 
 protected
