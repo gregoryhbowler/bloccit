709a55e2b51dfdf79e53317d9cd22b91679ae58a
gregoryhbowler <greg@brudmusic.com>
2015-07-06 14:56:25 -0700

    fixed image display -- parentheses problem

--- db/schema.rb
+++ db/schema.rb
@@ -26,11 +26,9 @@ ActiveRecord::Schema.define(version: 20150706181227) do
     t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "user_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
