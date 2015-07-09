9eb707453d8ae2cbebb2e6e4a2054700326e1ca7
gregoryhbowler <greg@brudmusic.com>
2015-07-08 20:32:03 -0700

    modified summaries controller

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -1,8 +1,30 @@
 class SummariesController < ApplicationController
   def new
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts
+    @summary = Summary.new
+    authorize @summary
+  end
+
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts
+    @summary = Summary.new
+    authorize @summary
+    if @summary.save
+      flash[:notice] = "Your post summary was saved."
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "Sorry. There was an error saving your summary. Please try again."
+      render :new
+    end
   end
 
   def show
+    @summary = Summary.find(params[:id])
+    @post = Post.find(params[:post_id])
+    @topic = Topic.find(params[:topic_id])
+    authorize @summary
   end
 
   def edit
