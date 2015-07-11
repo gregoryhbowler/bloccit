a9732a08330982bfd079cac07cd8542f7fb01f0c
gregoryhbowler <greg@brudmusic.com>
2015-07-11 16:04:56 -0700

    checkpoint 50 mocking rspec complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -25,9 +25,13 @@ class Post < ActiveRecord::Base
     update_attribute(:rank, new_rank)
   end
 
+  def create_vote
+    user.votes.create(value: 1, post: self)
+  end
+
 validates :title, length: { minimum: 5 }, presence: true
 validates :body, length: { minimum: 20 }, presence: true
-# validates :topic, presence: true
-# validates :user, presence: true
+validates :topic, presence: true
+validates :user, presence: true
   
 end
